cb4b6a4eb14c3e9b20032044ab57c70da37757f3
LatteEater <tanitpong.wat@mail.pbru.ac.th>
2016-06-07 16:00:32 +0700

    SQL Lite DELETE Finish

--- app/src/main/java/st_app_pbru/latteeater/itpbru2/MainActivity.java
+++ app/src/main/java/st_app_pbru/latteeater/itpbru2/MainActivity.java
@@ -1,6 +1,7 @@
 package st_app_pbru.latteeater.itpbru2;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,7 +20,18 @@ public class MainActivity extends AppCompatActivity {
         //Test Add New User
         //myManager.addNewUser("123","name","sur","User","Pass");
 
+        //Delete ALL SQLite
+        deleteAllSQLite();
+
     } // nMain method
+
+    private void deleteAllSQLite() {
+
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        sqLiteDatabase.delete(MyManager.user_table, null, null);
+
+    }//nDeleteALLSQLite
 
     public void clickSignUpMain(View view) {
         startActivity(new Intent(MainActivity.this, SignUpActivity.class));
