a49d868169e8f4b0145f4fba9fb89e3143b2847d
LatteEater <tanitpong.wat@mail.pbru.ac.th>
2016-06-07 16:42:11 +0700

    Synchronize Successful

--- app/src/main/java/st_app_pbru/latteeater/itpbru2/MainActivity.java
+++ app/src/main/java/st_app_pbru/latteeater/itpbru2/MainActivity.java
@@ -1,14 +1,23 @@
 package st_app_pbru.latteeater.itpbru2;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 public class MainActivity extends AppCompatActivity {
     //Explicit
     private MyManager myManager;
+    private static final String urlJSON = "http://swiftcodingthai.com/pbru2/get_user_master.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +31,64 @@ public class MainActivity extends AppCompatActivity {
 
         //Delete ALL SQLite
         deleteAllSQLite();
+        mySynJSON();
 
-    } // nMain method
+    } // nMain Method
+
+    private void mySynJSON() {
+
+        ConnectedUserTABLE connectedUserTABLE = new ConnectedUserTABLE(this);
+        connectedUserTABLE.execute();
+
+    }//nSynJSON
+
+    //Create Inner Class
+    private class ConnectedUserTABLE extends AsyncTask<Void, Void, String> {
+
+        private Context context;
+        private ProgressDialog progressDialog;
+
+        public ConnectedUserTABLE(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+           progressDialog = ProgressDialog.show(context, "Synchronize Server", "Please Wait...  Process Synchronize");
+        }//nOnPre
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urlJSON).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            } catch (Exception e) {
+                Log.d("7June","error DoIn==> "+e.toString()); //log Debugging
+            }
+
+            return null;
+        }//nDoInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            try {
+                progressDialog.dismiss();
+                Log.d("7June", "JSON==>"+s);
+            } catch (Exception e) {
+            e.printStackTrace();
+            }
+
+        }//nOnPostExecute
+    } //nConnected Class
 
     private void deleteAllSQLite() {
 
