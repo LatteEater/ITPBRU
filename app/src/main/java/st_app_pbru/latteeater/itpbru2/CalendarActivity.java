83cb348d69f4811fb5fa12c7d9584f9ed82a287a
LatteEater <tanitpong.wat@mail.pbru.ac.th>
2016-06-08 13:59:50 +0700

    Design UI Upload Finish

--- app/src/main/java/st_app_pbru/latteeater/itpbru2/CalendarActivity.java
+++ app/src/main/java/st_app_pbru/latteeater/itpbru2/CalendarActivity.java
@@ -2,6 +2,7 @@ package st_app_pbru.latteeater.itpbru2;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CalendarView;
@@ -37,21 +38,34 @@ public class CalendarActivity extends AppCompatActivity {
         builder.setIcon(R.drawable.doremon48);
         builder.setTitle(strDate);
         builder.setMessage("Income Or Outcome ?");
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+        builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.setNegativeButton("Outcome", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                Intent intent = new Intent(CalendarActivity.this, UploadAccount.class);
+                intent.putExtra("Login", getIntent().getStringArrayExtra("Login"));
+                intent.putExtra("InOut", 1);
+                startActivity(intent);
+
                 dialogInterface.dismiss();
             }
         });
         builder.setPositiveButton("Income", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.dismiss();
-            }
-        });
-        builder.setNeutralButton("Outcome", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
+
+                Intent intent = new Intent(CalendarActivity.this, UploadAccount.class);
+                intent.putExtra("Login", getIntent().getStringArrayExtra("Login"));
+                intent.putExtra("InOut", 0);
+                startActivity(intent);
+
                 dialogInterface.dismiss();
             }
         });
---
+++ app/src/main/java/st_app_pbru/latteeater/itpbru2/UploadAccount.java
@@ -0,0 +1,16 @@
+package st_app_pbru.latteeater.itpbru2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UploadAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_account);
+
+
+
+    }//n Main Method
+}//n MainClass
